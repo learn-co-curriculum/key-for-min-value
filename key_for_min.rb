5a2d2b961f7b5f25317d2242240c19495b8c1f1b
Arel English <arelenglish@gmail.com>
2014-02-09 23:48:19 -0500

    added solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-  # code goes here
+  min_value = hash.min_by {|k,v| v}
+  unless min_value == nil 
+    min_value.first 
+  end
 end
