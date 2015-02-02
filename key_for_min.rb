f1533d06f0a71ed69cfbbfe04176a0fd931646a0
Katie Hoffman <ktahoffman@gmail.com>
2015-02-02 00:10:25 -0500

    rename variable

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
+def key_for_min_value(name_hash)
   # code goes here
 end
