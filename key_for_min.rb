5b39b694aa87ebcfa6a89ed8d8e694cafac1b6c2
ipc103 <iancandysemail@gmail.com>
2016-01-05 10:40:54 -0500

    remove infinity from solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,13 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(hash)
-  min_value = hash.min_by {|k,v| v}
-  unless min_value == nil 
-    min_value.first 
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
   end
+  lowest_key
 end
