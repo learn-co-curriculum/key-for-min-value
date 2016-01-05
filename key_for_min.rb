5ef8507158fa6d6a1b8499b494bc9d418c8d055c
ipc103 <iancandysemail@gmail.com>
2016-01-05 10:40:54 -0500

    remove infinity from solution

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(hash)
   lowest_key = nil
-  lowest_value = Float::INFINITY
+  lowest_value = nil
   hash.each do |k, v|
-    if v < lowest_value
+    if lowest_value == nil || v < lowest_value
       lowest_value = v
       lowest_key = k
     end
