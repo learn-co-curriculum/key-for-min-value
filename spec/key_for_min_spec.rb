7649c8b7cf2199ec38a2ac15880c8081f3a05a8b
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-20 15:54:53 -0400

    Add check that certain methods aren't called

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -1,4 +1,45 @@
 describe "smallest hash value" do
+
+  it "does not call the `#keys` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:keys)
+
+    key_for_min_value(hash)
+  end
+
+  it "does not call the `#values` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:values)
+
+    key_for_min_value(hash)
+  end
+
+  it "does not call the `#min` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:min)
+
+    key_for_min_value(hash)
+  end
+
+  it "does not call the `#sort` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:sort)
+
+    key_for_min_value(hash)
+  end
+
+  it "does not call the `#min_by` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:min_by)
+
+    key_for_min_value(hash)
+  end
+
   it "returns the key of the smallest hash value" do
     expect(key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})).to eq(:adam)
   end
