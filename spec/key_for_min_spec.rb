9a01ecf249dcf5dd788a83a29ff49a16a6ad87ef
Steven G. Harms <steven.harms@flatironschool.com>
2019-01-02 13:13:51 -0500

    Ensure sort_by is not used

--- spec/key_for_min_spec.rb
+++ spec/key_for_min_spec.rb
@@ -32,6 +32,14 @@ describe "smallest hash value" do
     key_for_min_value(hash)
   end
 
+  it "does not call the `#sort_by` method" do
+    hash = {:blake => 500, :ashley => 2, :adam => 1}
+
+    expect(hash).to_not receive(:sort_by)
+
+    key_for_min_value(hash)
+  end
+
   it "does not call the `#min_by` method" do
     hash = {:blake => 500, :ashley => 2, :adam => 1}
 
