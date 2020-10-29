ddd50efb20ced2be487002ab11b388f9fe121430
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-29 01:34:42 -0500

    commit tree a1b27a5617ed63b930eacd3b849ace804cb3bc83

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    # code goes here
+    min_value = nil
+    min_key = nil
+    name_hash.each do |k, v|
+      if min_value == nil
+        min_value = v
+        min_key = k
+      elsif min_value > v
+        min_value = v
+        min_key = k
+      end
+    end
+    min_key
 end
